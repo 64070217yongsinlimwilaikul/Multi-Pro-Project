5c3388713a745f1def3f2a3e4b033caf9d520161
Waradon N <31185649+WaradonN@users.noreply.github.com>
2023-09-05 09:30:45 +0700

    minor changes again

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = -100; /* change how early or late you want the element to start revealing */
+      var elementVisible = -50; /* change how early or late you want the element to start revealing */
   
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
