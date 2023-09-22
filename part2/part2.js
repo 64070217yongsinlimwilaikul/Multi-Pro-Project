9c5ca15775b319cdc22eb8211cc29286d1db12a6
64070217yongsinlimwilaikul <64070217@it.kmitl.ac.th>
2023-09-22 20:29:50 +0700

    oak

--- part2/part2.js
+++ part2/part2.js
@@ -1,10 +1,10 @@
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
-  
+    // อันนี้คือความเร็วการ fade โดยกำหนดตัวเลขที่ตัวแปรข้างล่างนะ  เหมาะนำไปใช้กับ tag ที่ใช้ความเร็วเท่าๆกัน//
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 50; /* change how early or late you want the element to start revealing */
+      var elementVisible = 50; /* change how early or late you want the element to start revealing ยิ่งเลขมากยิ่งโผล่มาช้า เลขน้อยโผล่เร็ว(ติดลบด้วย)*/
   
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
@@ -13,12 +13,12 @@ function reveal() {
       }
     }
     
-
+    // อันนี้คือความเร็วการ fade โดยอิงจาก dataset.fade นะ เหมาะกับการเฉพาะเจาะจงกับ tag ที่ต้องการกำหนดความเร็ว fade เอง//
     var reveals2 = document.querySelectorAll(".reveal-2");
     for (var i = 0; i < reveals2.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals2[i].getBoundingClientRect().top;
-        var elementVisible = reveals2[i].dataset.fade; //ความเร็ว fade ไปแก้ที่ data set ของ html
+        var elementVisible = reveals2[i].dataset.fade; //ความเร็ว fade ไปแก้ที่ data set ของ html เลขมากโผล่ช้า เลขน้อยโผล่เร็ว
     
         if (elementTop < windowHeight - elementVisible) {
           reveals2[i].classList.add("active");
