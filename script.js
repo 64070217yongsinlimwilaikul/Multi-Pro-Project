69a78ae9502c1dd327be58e31c5bbe127fb9bd6f
64070237 <88420048+64070237@users.noreply.github.com>
2023-10-05 14:32:47 +0700

    Fix image.
    Fix images a little bit. (90% done. I think.)

--- script.js
+++ script.js
@@ -29,11 +29,21 @@ function showSection(sec) {
   let seclist = []
   if(sec == 1){seclist = [9,10,11,12,13,14,15,16,17,18]}
   else if(sec == 2){seclist = [19,20,21,22]}
-  else if(sec == 3){seclist = [23,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]}
-  else if(sec == 4){seclist = [24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]}
+  else if(sec == 3){seclist = [23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45]}
 
   for (let i in seclist) {
     document.querySelector('#section-' + seclist[i] ).classList.remove("none");
+  }
+}
+
+function showImage(num){
+  if(num == 1){ 
+    document.querySelector('#image23-1').classList.remove("none");
+    document.querySelector('#image23-2').classList.add("none");
+  }
+  else if(num ==2){
+    document.querySelector('#image23-1').classList.add("none");
+    document.querySelector('#image23-2').classList.remove("none");
   }
 }
 
