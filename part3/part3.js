3b74acb892e316043b00205288963441d629473d
64070237 <88420048+64070237@users.noreply.github.com>
2023-10-08 09:23:23 +0700

    Fix part3
    Fix part3.

--- part3/part3.js
+++ part3/part3.js
@@ -29,7 +29,7 @@ function showSection(sec) {
   let seclist = []
   if(sec == 1){seclist = [9,10,11,12,13,14,15,16,17,18]}
   else if(sec == 2){seclist = [19,20,21,22]}
-  else if(sec == 3){seclist = [23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46]}
+  else if(sec == 3){seclist = [23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47]}
 
   for (let i in seclist) {
     document.querySelector('#section-' + seclist[i] ).classList.remove("none");
