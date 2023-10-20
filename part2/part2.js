80c6553a1014e355c4d037bdf0e0fd7bb0563728
64070217yongsinlimwilaikul <64070217@it.kmitl.ac.th>
2023-10-20 19:17:32 +0700

    oak

--- part2/part2.js
+++ part2/part2.js
@@ -1,6 +1,4 @@
 function reveal() {
-  var x = document.querySelector("#section-5");
-  console.log(window.pageYOffset)
   var reveals = document.querySelectorAll(".reveal");
   // อันนี้คือความเร็วการ fade โดยกำหนดตัวเลขที่ตัวแปรข้างล่างนะ  เหมาะนำไปใช้กับ tag ที่ใช้ความเร็วเท่าๆกัน//
   for (var i = 0; i < reveals.length; i++) {
@@ -27,6 +25,50 @@ function reveal() {
     } else {
       reveals2[i].classList.remove("active");
     }
+  }
+
+  var sec5 = document.querySelector("#section-5");
+  var reveals3 = document.querySelectorAll(".reveal-3");
+  var sec5Height = sec5.offsetHeight;
+  var sec5Top = sec5.getBoundingClientRect().top;
+  var heightOfRevels = sec5Height / reveals3.length;
+  var sec5Visible = Math.ceil((-sec5Top - 100)/ heightOfRevels);
+  console.log(sec5Visible)
+  if(sec5Visible >= reveals3.length){
+    sec5Visible = reveals3.length;
+    document.querySelector("#book-text").classList.add("active");
+  }
+  else if(sec5Visible <= 0){
+    sec5Visible = 0;
+  }
+  else if(sec5Visible < reveals3.length){
+    document.querySelector("#book-text").classList.remove("active");
+  }
+  for (var i = 0; i < reveals3.length; i++) {
+    reveals3[i].classList.remove("active");
+  }
+  for (var i = 0; i < sec5Visible; i++) {
+    reveals3[i].classList.add("active");
+  }
+}
+function fixed() {
+  var detailsBook = document.querySelector(".all-details-book");
+  var divToFix = document.querySelector("#section-5");
+  var elementTop = divToFix.getBoundingClientRect().top;
+  var elementBottom = divToFix.getBoundingClientRect().bottom;
+
+  if (elementTop <= 0 & elementBottom > window.innerHeight) {
+    detailsBook.style.position = 'fixed';
+    detailsBook.style.top = '25%';
+    detailsBook.style.bottom = '25%';
+  }
+  else if (elementBottom <= window.innerHeight) {
+    var bottom = ((window.innerHeight - elementBottom) * (100 / window.innerHeight));
+    detailsBook.style.top = -bottom + "%";
+    detailsBook.style.bottom = bottom + "%";
+  }
+  else {
+    detailsBook.style.position = 'static';
   }
 }
 window.addEventListener("scroll", reveal);
